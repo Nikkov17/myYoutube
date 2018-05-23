0d26b9b3d7df365b6c326da5616a9573ecee65dc
Kovalyov Nikolai <kovalyov.k51@gmail.com>
2018-05-23 15:16:28 +0300

    fix scroll

--- js/infiniteScroll.js
+++ js/infiniteScroll.js
@@ -1,11 +1,10 @@
 import sendFetch from './sendFetch';
 
 export default function () {
-  const pageHeight = document.documentElement.offsetHeight;
-  const windowHeight = window.innerHeight;
-  const scroll = window.scrollY || window.pageYOffset || document.body.scrollTop;
-  const scrollTop = document.documentElement.scrollTop || 0;
-  const scrollPosition = scroll + (document.documentElement && scrollTop);
+    let pageHeight = document.documentElement.offsetHeight,
+        windowHeight = window.innerHeight,
+        scrollPosition = window.scrollY || window.pageYOffset || document.body.scrollTop + (document.documentElement && document.documentElement.scrollTop || 0);
+
 
   if (pageHeight - 1 <= windowHeight + scrollPosition) {
     document.querySelector('.progress-bar').classList.add('rotate');
